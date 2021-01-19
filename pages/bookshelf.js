187e609841bc98f75426d90f263da0ca24aad11d
Mitul Shah <mitulxshah@gmail.com>
2021-01-18 21:19:06 -0500

    designing page

---
+++ pages/bookshelf.js
@@ -0,0 +1,13 @@
+import Shell from "@/components/Shell";
+import Bookshelf from "@/components/bookshelf/Bookshelf";
+
+export default function bookshelf() {
+    return (
+      <Shell>
+        <Bookshelf />
+      </Shell>
+    );
+}
+
+
+
