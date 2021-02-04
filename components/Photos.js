260cd1452e39ce40a5f341b39f9be6922b601419
Mitul Shah <mitulxshah@gmail.com>
2021-02-03 22:20:56 -0500

    font issue

--- components/Photos.js
+++ components/Photos.js
@@ -1,40 +1,40 @@
-import { Box, Image, HStack } from "@chakra-ui/react"
-
+import { Box, Image, HStack } from "@chakra-ui/react";
 
 const Photos = ({ images }) => {
-
-  console.log(images)
-  
-    return (
-      <HStack
-        overflowX="scroll"
-        w="100%"
-        flexWrap="nowrap"
-        sx={{ scrollSnapType: "x proximity", scrollbarWidth: "none", '&::-webkit-scrollbar': {
-    background: "transparent", 
-} }}
-      >
-        {images?.map((i) => {
-          return (
-            <Box
-              minW="300px"
-              objectFit="scale-down"
-              key={i.id}
-              sx={{
-                scrollSnapAlign: "start",
-                background: "rgba( 255, 255, 255, 0.00 )",
-                boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.08 );",
-                backdropFilter: "blur( 0.5px );",
-                borderRadius: "2px",
-                border: "1px solid rgba( 255, 255, 255, 0.18 );",
-              }}
-            >
-              <Image src={i.src} alt={i.alt} />
-            </Box>
-          );
-        })}
-      </HStack>
-    );
-}
+  return (
+    <HStack
+      overflowX="scroll"
+      w="100%"
+      flexWrap="nowrap"
+      sx={{
+        scrollSnapType: "x proximity",
+        scrollbarWidth: "none",
+        "&::-webkit-scrollbar": {
+          background: "transparent",
+        },
+      }}
+    >
+      {images?.map((i) => {
+        return (
+          <Box
+            minW="300px"
+            objectFit="scale-down"
+            key={i.id}
+            sx={{
+              scrollSnapAlign: "start",
+              background: "rgba( 255, 255, 255, 0.00 )",
+              boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.08 );",
+              backdropFilter: "blur( 0.5px );",
+              borderRadius: "2px",
+              border: "1px solid rgba( 255, 255, 255, 0.18 );",
+            }}
+          >
+            <Image src={i.src} alt={i.alt} />
+          </Box>
+        );
+      })}
+    </HStack>
+  );
+};
 
 export default Photos;
