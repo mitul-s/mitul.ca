0c84e4416edbae02384f5dd74498ad24a0927e19
Mitul Shah <mitulxshah@gmail.com>
2021-06-15 23:42:20 -0400

    some more colour control over mobile nav

--- components/Shell.js
+++ components/Shell.js
@@ -6,6 +6,7 @@ import {
   Heading,
   HStack,
   Icon,
+  Divider,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import Navigation from "@/components/Navigation";
@@ -18,7 +19,8 @@ const NavBtn = ({ children }) => {
     <Button
       bgColor="#040415"
       color="white"
-      // border="1px solid white"
+      borderWidth="0.3px"
+      borderColor="trueGray.500"
       w={28}
       rounded="3xl"
       shadow="lg"
@@ -28,54 +30,70 @@ const NavBtn = ({ children }) => {
   );
 };
 
-// const Nav = () => {
-//   return (
-//     <Box
-//       position="fixed"
-//       bottom="12"
-//       width="min(60ch, calc(100% - 64px))"
-//       // m="0 auto"
-//       zIndex="99999"
-//       // bgColor="black"
-//       rounded="md"
-//       // py={4}
-//       sx={{
-//         left: "50%",
-//         transform: "translateX(-50%)",
-//       }}
-//     >
-//       <Box w="fit-content" m="0 auto">
-//         <HStack spacing={3} alignItems="center" justifyContent="center">
-//           <NavBtn>About</NavBtn>
-//           <NavBtn>Journal</NavBtn>
-//           <Button bgColor="#F15223" w={0} rounded="full" shadow="lg">
-//             <Icon as={HouseSimple} />
-//           </Button>
-//           <Button bgColor="trueGray.800" w={0} rounded="full" shadow="lg">
-//             <Icon as={Sun} />
-//           </Button>
-//         </HStack>
-//       </Box>
-//     </Box>
-//   );
-// };
+const Nav = () => {
+  return (
+    <Box
+      position="fixed"
+      bottom="12"
+      width="min(60ch, calc(100% - 64px))"
+      // m="0 auto"
+      zIndex="99999"
+      bgColor="trueGray.900"
+      rounded="full"
+      p={2}
+      w="fit-content"
+      borderWidth="1px"
+      // py={4}
+      sx={{
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      <Box w="fit-content" m="0 auto">
+        <HStack spacing={3} alignItems="center" justifyContent="center">
+          <NavBtn>About</NavBtn>
+          <NavBtn>Journal</NavBtn>
+          <Box height="25px">
+            <Divider orientation="vertical" />
+          </Box>
+          <Button bgColor="#F15223" w={0} rounded="full" shadow="lg">
+            <Icon as={HouseSimple} />
+          </Button>
+          <Button bgColor="trueGray.800" w={0} rounded="full" shadow="lg">
+            <Icon as={Sun} />
+          </Button>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
 
 // const Nav = () => {
-//   return <Box>Hello</Box>;
+//   return (
+//   <Box h="100vh" margin="0 auto">
+//     <Flex
+//       mt={24}
+//       w="auto"
+//       position="fixed"
+//       alignItems="center"
+//       justifyContent="center"
+//       flexDirection="column"
+//     >
+//       Hello
+//     </Flex>
+//   </Box>);
 // };
 
 const Shell = ({ children }) => {
   return (
-    <>
+    <Grid gridTemplateColumns="1fr min(60ch, calc(100% - 64px)) 1fr" gridColumnGap="32px">
       <Navigation />
-      {/* <Nav /> */}
-      <Box className={styles.grid}>
-        <Box as="main">
-          {children}
-          <Footer />
-        </Box>
+      <Nav />
+      <Box as="main">
+        {children}
+        <Footer />
       </Box>
-    </>
+    </Grid>
   );
 };
 
