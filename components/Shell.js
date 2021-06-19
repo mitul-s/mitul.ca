67869e80bd965c00d4c69d6f5fe48bfcbba305f1
Mitul Shah <mitulxshah@gmail.com>
2021-06-18 20:12:48 -0400

    tooltip, colours, borders, blur. lots of progress.

--- components/Shell.js
+++ components/Shell.js
@@ -6,6 +6,8 @@ import {
   Heading,
   HStack,
   Icon,
+  Circle,
+  Tooltip,
   Divider,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
@@ -17,13 +19,22 @@ import { HouseSimple, Sun } from "phosphor-react";
 const NavBtn = ({ children }) => {
   return (
     <Button
-      bgColor="#040415"
+      bgColor="rgba(13, 16, 19, 1)"
       color="white"
       borderWidth="0.3px"
-      borderColor="trueGray.500"
+      // borderColor="trueGray.500"
       w={28}
       rounded="3xl"
       shadow="lg"
+      fontSize="md"
+      fontWeight="regular"
+      letterSpacing={0.5}
+      _hover={{
+        bgColor: "#000",
+      }}
+      sx={{
+        fontSmooth: "antialiased"
+      }}
     >
       {children}
     </Button>
@@ -37,31 +48,76 @@ const Nav = () => {
       bottom="12"
       width="min(60ch, calc(100% - 64px))"
       // m="0 auto"
-      zIndex="99999"
-      bgColor="trueGray.900"
       rounded="full"
-      p={2}
+      zIndex="99"
       w="fit-content"
       borderWidth="1px"
       // py={4}
       sx={{
         left: "50%",
-        transform: "translateX(-50%)",
+        transform: "translateX(-50.9%)",
       }}
     >
-      <Box w="fit-content" m="0 auto">
+      <Box
+        w="fit-content"
+        m="0 auto"
+        p={2}
+        rounded="full"
+        bgColor="rgb(13, 16, 19, 0.45)"
+        className={styles.navBlur}
+      >
         <HStack spacing={3} alignItems="center" justifyContent="center">
           <NavBtn>About</NavBtn>
           <NavBtn>Journal</NavBtn>
           <Box height="25px">
             <Divider orientation="vertical" />
           </Box>
-          <Button bgColor="#F15223" w={0} rounded="full" shadow="lg">
+          <Button
+            bgColor="#F15223"
+            w={0}
+            rounded="full"
+            shadow="lg"
+            border="1px solid transparent"
+            role="group"
+            _hover={{
+              border: "1px",
+              borderColor: "#F15223",
+              bgColor: "rgb(241, 82, 35, 0.7)",
+            }}
+          >
             <Icon as={HouseSimple} />
           </Button>
-          <Button bgColor="trueGray.800" w={0} rounded="full" shadow="lg">
-            <Icon as={Sun} />
-          </Button>
+          <Tooltip
+            label="Change theme"
+            fontSize="xs"
+            letterSpacing={1}
+            placement="top"
+            bgColor="black"
+            color="white"
+            rounded="sm"
+            closeDelay={200}
+            // openDelay={300}
+            // mb={2}
+          >
+            <Circle
+              bgColor="trueGray.800"
+              size="40px"
+              rounded="full"
+              shadow="lg"
+              role="group"
+              transitionDuration="200ms"
+              border="1px solid transparent"
+              _hover={{
+                transform: "rotate(90deg)",
+                bgColor: "white",
+                color: "black",
+                border: "1px solid",
+                borderColor: "black"
+              }}
+            >
+              <Icon as={Sun} />
+            </Circle>
+          </Tooltip>
         </HStack>
       </Box>
     </Box>
