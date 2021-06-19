2eb29cf8b01720fa98d98ad005f1224fff039d72
Mitul Shah <mitulxshah@gmail.com>
2021-06-19 17:41:26 -0400

    refactored code and small cleanups

--- components/Navigation.js
+++ components/Navigation.js
@@ -30,147 +30,18 @@ import {
 import styles from ".././styles/sidebar.module.css";
 import NextLink from "next/link";
 
-const FooterIcon = ({ title, icon, link }) => (
-  <Link
-    href={link}
-    isExternal
-    data-splitbee-event={title}
-    data-splitbee-event-type="Social"
-  >
-    <Icon
-      fontSize="xl"
-      transition="150ms ease-in-out"
-      _hover={{ color: "cyan.600" }}
-      as={icon}
-    />
-  </Link>
-);
-
-const ToggleColorMode = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <IconButton
-      bg={colorMode === "light" ? "trueGray.100" : "trueGray.900"}
-      order={["1", null, 0]}
-      p={0}
-      onClick={toggleColorMode}
-      icon={colorMode === "light" ? <Moon /> : <Sun />}
-    />
-  );
-};
-
-const NavButton = React.forwardRef((props, ref) => {
-  const { colorMode } = useColorMode();
-  return (
-    <Button
-      // className="underline-animation"
-      colorScheme="trueGray"
-      fontSize="lg"
-      fontWeight="400"
-      sx={{
-        ":hover:after": {
-          transform: "scaleX(1)",
-          transformOrigin: "bottom left",
-        },
-      }}
-      as="a"
-      variant="link"
-      _hover={{
-        textDecoration: "none",
-        color: colorMode === "light" ? "blueGray.900" : "blueGray.50",
-      }}
-      _after={{
-        content: '""',
-        position: "absolute",
-        width: "100%",
-        transform: "scaleX(0)",
-        height: "1px",
-        bottom: 0,
-        left: 0,
-        borderRadius: 2,
-        backgroundColor: colorMode === "light" ? "blueGray.900" : "blueGray.50",
-        marginBottom: "-5px",
-        transformOrigin: "bottom right",
-        transition: "transform 0.35s ease-out",
-      }}
-      {...props}
-    >
-      {props.children}
-    </Button>
-  );
-});
-
-const NavLinks = ({ ...rest }) => (
-  <Stack spacing={4} alignItems={["center", null, "flex-end"]} {...rest}>
-    <NextLink href="/">
-      <NavButton>Home</NavButton>
-    </NextLink>
-    <NextLink href="/about">
-      <NavButton>About</NavButton>
-    </NextLink>
-    <NavButton disabled>
-      Bookshelf
-      <Badge ml={2}>WIP</Badge>
-    </NavButton>
-    <NavButton href="mailto:mitulxshah@gmail.com?subject=Hi Mitul">
-      Contact
-    </NavButton>
-  </Stack>
-);
-
-const Navigation = ({ styles }) => (
-  <Box m="0 auto">
-    <Box
-      display={["none", "none", "none", "flex"]}
-      mt={24}
-      w="auto"
-      position="fixed"
-      alignItems="flex-end"
-      justifyContent="space-between"
-      flexDirection="column"
-      h="30vh"
-    >
-      <ToggleColorMode />
-      <NavLinks direction={["row", null, "column"]} />
-      <HStack display={["none", null, "flex"]}>
-        <FooterIcon
-          title="twitter"
-          icon={TwitterLogo}
-          link="https://twitter.com/typicalmitul"
-        />
-        <FooterIcon
-          title="instagram"
-          icon={InstagramLogo}
-          link="https://instagram.com/typicalmitul"
-        />
-        <FooterIcon
-          title="github"
-          icon={GithubLogo}
-          link="https://github.com/mitul-s"
-        />
-        <FooterIcon
-          title="linkedin"
-          icon={LinkedinLogo}
-          link="https://www.linkedin.com/in/heymitul/"
-        />
-      </HStack>
-    </Box>
-  </Box>
-);
-
-
-const SocialButton = ({ icon, link }) => {
+const SocialButton = ({ title, icon, link }) => {
   const { colorMode } = useColorMode();
   return (
     <Circle
       as="a"
       href={link}
+      data-splitbee-event={title}
+      data-splitbee-event-type="Social"
       target="_blank"
       size="40px"
       bgColor={
-        colorMode === "light"
-          ? "rgba(245, 245, 245, 0.45)"
-          : "rgba(13, 16, 19, 0.45)"
+        colorMode === "light" ? "rgba(245, 245, 245, 1)" : "rgba(13, 16, 19, 1)"
       }
       transitionDuration="250ms"
       _hover={{
@@ -184,6 +55,75 @@ const SocialButton = ({ icon, link }) => {
 };
 
 
+const SocialPopover = () => {
+  const { colorMode } = useColorMode();
+  return (
+    <Popover closeOnBlur={true} returnFocusOnClose={false}>
+      <PopoverTrigger>
+        <IconButton
+          bgColor="indigo.600"
+          color="whiteAlpha.900"
+          rounded="full"
+          borderColor="indigo.400"
+          borderWidth="0.3px"
+          icon={<List />}
+          _hover={{
+            border: "1px",
+            borderColor: "indigo.600",
+            bgColor: "indigo.700",
+            transform: "rotate(90deg)",
+          }}
+        />
+      </PopoverTrigger>
+      <PopoverContent
+        width="50px"
+        rounded="full"
+        height="auto"
+        mb={2}
+        bg="none"
+      >
+        <Flex
+          direction="column"
+          justifyContent="space-between"
+          alignItems="center"
+          rounded="full"
+          height={48}
+          px={3}
+          py={1}
+          bgColor={
+            colorMode === "light"
+              ? "rgba(245, 245, 245, 0.45)"
+              : "rgba(13, 16, 19, 0.45)"
+          }
+          className={styles.navBlur}
+        >
+          <SocialButton
+            title="Twitter"
+            icon={TwitterLogo}
+            link="https://twitter.com/typicalmitul"
+          />
+          <SocialButton
+            title="Instagram"
+            icon={InstagramLogo}
+            link="https://instagram.com/typicalmitul"
+          />
+          <SocialButton
+            title="GitHub"
+            icon={GithubLogo}
+            link="https://github.com/mitul-s"
+          />
+          <SocialButton
+            title="Linkedin"
+            icon={LinkedinLogo}
+            link="https://linkedin.com/in/heymitul"
+          />
+        </Flex>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+
 const ToggleThemeBtn = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
@@ -192,31 +132,37 @@ const ToggleThemeBtn = () => {
       fontSize="xs"
       letterSpacing={1}
       placement="top"
-      bgColor="black"
-      color="white"
       rounded="sm"
     >
       <IconButton
-        bg={colorMode === "light" ? "trueGray.100" : "trueGray.900"}
-        // size="40px"
+        bgColor={
+          colorMode === "light"
+            ? "rgb(245, 245, 245, 1)"
+            : "rgba(13, 16, 19, 1)"
+        }
+        color={colorMode === "light" ? "black" : "white"}
+        outline="none"
         rounded="full"
         transitionDuration="200ms"
         borderWidth="0.3px"
         onClick={toggleColorMode}
         icon={colorMode === "light" ? <Moon /> : <Sun />}
         _hover={{
-          bgColor: `${colorMode === "light" ? "rgb(245, 245, 245, 0.5)" : "rgb(38, 38, 38, 0.5)"}`,
-          transform: "rotate(90deg)",
+          borderColor: `${colorMode === "light" ? "black" : "whiteAlpha.800"}`,
+          transform: "rotate(45deg)",
           border: "1px solid",
           borderColor: "whiteAlpha.800",
+        }}
+        _focus={{
+          outline: "none",
+        }}
+        _active={{
+          outline: "none",
         }}
       />
     </Tooltip>
   );
 }
-
-
-
 
 
 const NavBtn = React.forwardRef((props, ref) => {
@@ -236,7 +182,6 @@ const NavBtn = React.forwardRef((props, ref) => {
       fontWeight="regular"
       letterSpacing={0.5}
       _hover={{
-        // bgColor: "#000",
         borderColor: `${colorMode === 'light' ? "black" : "whiteAlpha.800"}`,
       }}
       sx={{
@@ -290,57 +235,7 @@ const Nav = () => {
             <Divider colorScheme="blackAlpha" orientation="vertical" />
           </Box>
           <ToggleThemeBtn />
-          <Popover>
-            <PopoverTrigger>
-              <IconButton
-                bgColor="#F15223"
-                rounded="full"
-                borderColor="#F15223"
-                borderWidth="0.3px"
-                icon={<List />}
-                _hover={{
-                  border: "1px",
-                  borderColor: "#F15223",
-                  bgColor: "rgb(241, 82, 1, 0.5)",
-                  transform: "rotate(90deg)",
-                }}
-              />
-            </PopoverTrigger>
-            <PopoverContent
-              width="50px"
-              rounded="full"
-              height="auto"
-              mb={2}
-              px={3}
-              py={1}
-              bgColor={
-                colorMode === "light"
-                  ? "rgba(245, 245, 245, 0.45)"
-                  : "rgba(13, 16, 19, 0.45)"
-              }
-              className={styles.navBlur}
-            >
-              <Flex
-                direction="column"
-                justifyContent="space-between"
-                alignItems="center"
-                height={40}
-              >
-                <SocialButton
-                  icon={TwitterLogo}
-                  link="https://twitter.com/typicalmitul"
-                />
-                <SocialButton
-                  icon={InstagramLogo}
-                  link="https://instagram.com/typicalmitul"
-                />
-                <SocialButton
-                  icon={GithubLogo}
-                  link="https://github.com/mitul-s"
-                />
-              </Flex>
-            </PopoverContent>
-          </Popover>
+          <SocialPopover />
         </HStack>
       </Box>
     </Box>
