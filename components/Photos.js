af4740bd52045db6d05b41f0e28c847880bc216a
Mitul Shah <mitulxshah@gmail.com>
2021-07-17 15:15:54 -0400

    Photo are now expandable on click

--- components/Photos.js
+++ components/Photos.js
@@ -1,4 +1,4 @@
-import { Box, Image, HStack } from "@chakra-ui/react";
+import { Box, Image, HStack, useColorMode } from "@chakra-ui/react";
 import styles from "@/styles/sidebar.module.css";
 import { useState } from "react";
 
@@ -25,39 +25,35 @@ const Photos = ({ images, ...restProps }) => {
         }}
       >
         {images?.map((i) => {
-          const [imgWidth, setImgWidth] = useState("240px");
-          const [widthOn, setWidthOn] = useState(false);
-          const toggleWidth = () => {
-            if (widthOn) {
-              setWidthOn(false);
-              setImgWidth("240px");
-            } else {
-              setWidthOn(true);
-              setImgWidth("400px");
-            }
-          };
+          const [width, setWidth] = useState(false);
+          const toggleWidth = () => { width ? setWidth(false) : setWidth(true) };
+          const { colorMode } = useColorMode();
           return (
             <Box
               key={i.id}
-              minW={imgWidth}
+              minW={width ? "500px" : "240px"}
               height="100%"
               objectFit="contain"
-              transition="300ms"
+              transition="all 250ms cubic-bezier(0.4, 0, 0.2, 1);"
               marginRight={4}
-              onClick={(i) => {
-                toggleWidth(i);
-              }}
+              onClick={(i) => toggleWidth(i)}
               sx={{
                 // scrollSnapAlign: "start end",
                 background: "rgba( 255, 255, 255, 0.15 )",
-                boxShadow: "0 5px 12px rgb(0 0 0 / 20%)",
+                boxShadow: "0 5px 12px rgb(0 0 0 / 30%)",
                 backdropFilter: "blur(0.5px)",
                 borderRadius: "2px",
                 border: "1px solid rgba( 255, 255, 255, 0.20 )",
               }}
+              _hover={{
+                background:
+                  colorMode === "dark"
+                    ? "rgba(245, 245, 245, 1)"
+                    : "rgba(13, 16, 19, 1)",
+              }}
               className={`${styles.marginHandle} link`}
             >
-              <Image src={i.src} alt={i.alt} transition="300ms" />
+              <Image src={i.src} alt={i.alt} />
             </Box>
           );
         })}
