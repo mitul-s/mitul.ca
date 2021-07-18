df2a533d93e4262238602c43f6e2a62c4d2ee79d
Mitul Shah <mitulxshah@gmail.com>
2021-07-17 22:46:36 -0400

    removed animated cursor code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,20 +3,11 @@ import "../styles/globals.css";
 import theme from "@/styles/theme.js";
 import { DefaultSeo } from "next-seo";
 import SEO from "../../next-seo.config";
-import dynamic from "next/dynamic";
-
-// const AnimatedCursor = dynamic(
-//   () => import("../node_modules/react-animated-cursor"),
-//   {
-//     ssr: false,
-//   }
-// );
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <DefaultSeo {...SEO} />
-      {/* <AnimatedCursor color="94, 234, 212" /> */}
       <Component {...pageProps} />
     </ChakraProvider>
   );
