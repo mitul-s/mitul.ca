ac4a3f7e205954f421d5cbe61599aefb1c4f619a
Mitul Shah <mitulxshah@gmail.com>
2022-01-15 00:54:01 -0500

    Revert "Merge pull request #46 from mitul-s/revert-43-literal-api"
    This reverts commit d86d8401ccfc28c5e5e2222c92fe2db14232d576, reversing
    changes made to f8f2f19d6e06962bd0dc32f1cab10db0b6205129.

---
+++ src/lib/literal.js
@@ -0,0 +1,71 @@
+
+const LITERAL_ENDPOINT = `https://literal.club/graphql/`;
+
+  export const getAccessToken = async () => {
+    const response = await fetch(LITERAL_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+            mutation login($email: String!, $password: String!) {
+                login(email: $email, password: $password) {
+                    token
+                }
+            }
+        `,
+        variables: {
+          email: process.env.LITERAL_USER_EMAIL,
+          password: process.env.LITERAL_USER_PASSWORD,
+        },
+      }),
+    }).then((res) => res.json());
+    return response.data.login.token;
+  };
+
+export const getShelves = async () => {
+    const access_token  = await getAccessToken();
+
+    return fetch(LITERAL_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query myReadingStates {
+            myReadingStates {
+              ...ReadingStateParts # find fragments below
+              book {
+                ...BookParts # find fragments below
+              }
+            }
+          }
+
+          fragment ReadingStateParts on ReadingState {
+            id
+            status
+            bookId
+            profileId
+            createdAt
+          }
+
+          fragment BookParts on Book {
+            id
+            slug
+            title
+            subtitle
+            description
+            cover
+            authors {
+              id
+              name
+            }
+          }
+            `,
+      }),
+    });
+};
+
