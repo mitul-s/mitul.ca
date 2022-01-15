757505a7d4906157810fd11bb7d5015a47287ae3
Mitul Shah <mitulxshah@gmail.com>
2022-01-14 23:42:15 -0500

    clean up

--- src/pages/api/books.js
+++ src/pages/api/books.js
@@ -1,10 +1,27 @@
-import { getShelves, getAccessToken } from "@/lib/getBooks";
+import { getShelves } from "@/lib/literal";
 
 export default async (_, res) => {
   
   const response = await getShelves();
-  const data = await response.json();
-  const books = data.data.myReadingStates.filter(book => book.status === "IS_READING");
+  const shelves = await response.json();
+  
+  const currentlyReading = shelves.data.myReadingStates.filter(shelf => shelf.status === "IS_READING");
+  const latestBooks = currentlyReading?.map((book) => {
+    return {       
+      slug: book.book.slug,
+      title: book.book.title, 
+      author: book.book.authors[0].name
+    };
+  });
 
-  return res.status(200).json({ books });
+  const title = latestBooks?.at(-1)?.title;
+  const author = latestBooks?.at(-1)?.author;
+  const slug = latestBooks?.at(-1)?.slug;
+  
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=60, stale-while-revalidate=30"
+  );
+
+  return res.status(200).json({ title, author, slug });
 }
