6ce251796c641b22e0ce6153eea04b6e976efd06
Mitul Shah <mitulxshah@gmail.com>
2023-10-23 00:20:27 -0400

    Update item.tsx

--- components/item.tsx
+++ components/item.tsx
@@ -6,7 +6,7 @@ const Item = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="p-4">
+    <div className="p-1 md:p-4">
       <h2 className="font-medium text-gray-11 mb-2">{heading}</h2>
       {children}
     </div>
