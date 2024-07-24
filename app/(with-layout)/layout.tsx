1e7fc26aac3add1c4edf1aa9b2aff842f625b711
mitul-s <mitulxshah@gmail.com>
2024-07-24 00:51:02 -0400

    Update layout.tsx

--- app/(with-layout)/layout.tsx
+++ app/(with-layout)/layout.tsx
@@ -1,4 +1,4 @@
-const Layout = ({ children }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return <div className="p-4 py-10 md:p-12 bg">{children}</div>;
 };
 export default Layout;
