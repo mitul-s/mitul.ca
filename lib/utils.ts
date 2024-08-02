60a741949cb18639c96b45ce761f3ce34d8e3285
Mitul Shah <mitulxshah@gmail.com>
2024-08-02 11:56:09 -0400

    Revert "mood definer  (#59)" (#60)
    This reverts commit 792499674fdd140bd8fcc071c887b819fdf44ab4.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,12 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function pick(object: Record<string, any>, keys: string[]) {
-  return keys.reduce((obj, key) => {
-    if (object && object.hasOwnProperty(key)) {
-      obj[key] = object[key];
-    }
-    return obj;
-  }, {} as Record<string, any>);
-}
