e631190b48355976bcf8938965d5a44269a190c4
mitul-s <mitulxshah@gmail.com>
2024-12-04 23:45:41 -0500

    Update layout.tsx

--- app/(without-root-layout)/p/layout.tsx
+++ app/(without-root-layout)/p/layout.tsx
@@ -2,40 +2,43 @@ import Link from "next/link";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-[#F9F2E2] text-gray-12 text-[16px]">
-      <nav className="max-w-[60ch] mx-auto w-full mt-6 flex gap-x-2.5 justify-between">
-        <div className="flex gap-x-2.5 items-center">
+    <>
+      <meta name="theme-color" content="#F9F2E2" />
+      <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-[#F9F2E2] text-gray-12 text-[16px]">
+        <nav className="max-w-[60ch] mx-auto w-full mt-6 flex gap-x-2.5 justify-between">
+          <div className="flex gap-x-2.5 items-center">
+            <Link
+              href="/"
+              className="underline underline-offset-4 decoration-from-font"
+            >
+              Home
+            </Link>
+            <Link
+              href="/visitors"
+              className="underline underline-offset-4 decoration-from-font"
+            >
+              Guestbook
+            </Link>
+          </div>
           <Link
-            href="/"
+            href="https://x.com/typicalmitul"
+            target="_blank"
+            rel="noopener noreferrer"
             className="underline underline-offset-4 decoration-from-font"
           >
-            Home
+            @typicalmitul
           </Link>
-          <Link
-            href="/visitors"
-            className="underline underline-offset-4 decoration-from-font"
-          >
-            Guestbook
-          </Link>
-        </div>
-        <Link
-          href="https://x.com/typicalmitul"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline underline-offset-4 decoration-from-font"
-        >
-          @typicalmitul
-        </Link>
-      </nav>
-      <main className="max-w-[60ch] mx-auto w-full space-y-6 pb-12">
-        {children}
-      </main>
-      <footer className="max-w-[60ch] mx-auto w-full text-left">
-        <p className="text-[10px] text-gray-10 font-medium">
-          from toronto, with love / typicalmitul
-        </p>
-      </footer>
-    </div>
+        </nav>
+        <main className="max-w-[60ch] mx-auto w-full space-y-6 pb-12">
+          {children}
+        </main>
+        <footer className="max-w-[60ch] mx-auto w-full text-left">
+          <p className="text-[10px] text-gray-10 font-medium">
+            from toronto, with love / typicalmitul
+          </p>
+        </footer>
+      </div>
+    </>
   );
 };
 
