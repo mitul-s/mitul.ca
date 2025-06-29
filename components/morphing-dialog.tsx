1075a07aa610481d46ccfac7b7adf8b24d355013
mitul-s <mitulxshah@gmail.com>
2025-06-29 00:25:35 -0400

    next 15

--- components/morphing-dialog.tsx
+++ components/morphing-dialog.tsx
@@ -20,7 +20,7 @@ export type MorphingDialogContextType = {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   uniqueId: string;
-  triggerRef: React.RefObject<HTMLDivElement>;
+  triggerRef: React.RefObject<HTMLDivElement | null>;
 };
 
 const MorphingDialogContext =
@@ -83,7 +83,7 @@ export type MorphingDialogTriggerProps = {
   children: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
-  triggerRef?: React.RefObject<HTMLDivElement>;
+  triggerRef?: React.RefObject<HTMLDivElement | null>;
 };
 
 function MorphingDialogTrigger({
